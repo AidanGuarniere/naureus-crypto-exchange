8bc9f8d66937309249485ddb6414ec62ae46264f
aidan <aidanguarniere@gmail.com>
2022-04-08 14:30:05 -0700

    basic react file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import { Navbar, Footer, Loader, Services, Transactions, Welcome } from './components'
 import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar/>
+      <Welcome/>
+    </div>
+    <Services/>
+    <Transactions/>
+    <Footer/>
     </div>
   )
 }
