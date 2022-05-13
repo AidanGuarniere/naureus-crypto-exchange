0cc2ac104b23f87103d60053635f21da478e4827
aidan <aidanguarniere@gmail.com>
2022-05-12 17:12:51 -0700

    Navbar and Welcome component UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
+import { ScreenContext } from "../contexts/ScreenContext";
 
 import logo from "../../images/logo.png";
 
@@ -9,6 +10,10 @@ const NavbarItem = ({ title, classProps }) => {
 };
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const { screen } = useContext(ScreenContext);
+
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -22,7 +27,38 @@ const Navbar = () => {
           Login
         </li>
       </ul>
-      <div className="flex relative"></div>
+      <div className="flex relative">
+        {toggleMenu ? (
+          <ul
+            className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+          >
+            <li className="text-xl w-full my-2">
+              <AiOutlineClose
+                className="text-white md:hidden cursor-pointer"
+                onClick={() => setToggleMenu(false)}
+              />
+            </li>
+            {["Market", "Exchange", "Tutorials", "Wallets"].map(
+              (item, index) => (
+                <NavbarItem
+                  key={item + index}
+                  title={item}
+                  classProps="my-2 text-lg"
+                />
+              )
+            )}
+            <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+              Login
+            </li>
+          </ul>
+        ) : (
+          <HiMenuAlt4
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+      </div>
     </nav>
   );
 };
